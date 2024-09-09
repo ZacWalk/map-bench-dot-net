b2bf4af253d71f8337b7a27f212cba933a306f72
zacwalker <zacwalker@ZAC-H1>
2024-09-09 11:27:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using map_bench_dot_net;
+
+var spec = Mix.ReadHeavy();
+var mix = spec.ToOps();
+var capacity = 1_000_000;
+var totalOps = capacity * 55;
+var prefill = capacity / 2;
+var totalKeys = prefill + (totalOps * spec.Insert / 100) + 100;
+
+List<Measurement> measurements = new();
+var keys = new Bench.Keys(totalKeys);
+
+for (var i = 0; i < Environment.ProcessorCount; i++)
+{
+    var config = new Bench.RunConfig
+    {
+        Threads = i + 1,
+        InitialCapacity = capacity,
+        TotalOps = totalOps,
+        Prefill = prefill
+    };
+
+    measurements.Add(Bench.RunWorkload(mix, config, keys, true));
+}
+
+var csvFilePath = "latency95.csharp.csv";
+
+using (var writer = new StreamWriter(csvFilePath))
+{
+    writer.WriteLine("name,total_ops,threads,latency");
+
+    foreach (var m in measurements)
+    {
+        var row = string.Format("{0},{1},{2},{3}",
+            "c#",
+            m.TotalOps,
+            m.ThreadCount,
+            m.Latency
+        );
+
+        writer.WriteLine(row);
+
+        var code_line = string.Format("Measurement {{ name: \"c#\", total_ops: {0}, thread_count: {1}, latency: {2} }},",
+            m.TotalOps,
+            m.ThreadCount,
+            m.Latency
+        );
+
+        Console.WriteLine(code_line);
+    }
+
+    
+}
+
+Console.WriteLine("CSV file written successfully!");
---
+++ bench.cs
@@ -0,0 +1,257 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace map_bench_dot_net;
+
+using KeyType = ulong;
+using ValueType = ulong;
+
+public struct Measurement
+{
+    /// <summary>
+    ///     A total number of operations.
+    /// </summary>
+    public long TotalOps { get; set; }
+
+    /// <summary>
+    ///     An average value of latency.
+    /// </summary>
+    public long Latency { get; set; }
+
+    /// <summary>
+    ///     A total number of threads.
+    /// </summary>
+    public long ThreadCount { get; set; }
+}
+
+public struct Mix
+{
+    /// <summary>
+    ///     The percentage of operations in the mix that are reads.
+    /// </summary>
+    public byte Read { get; set; }
+
+    /// <summary>
+    ///     The percentage of operations in the mix that are inserts.
+    /// </summary>
+    public byte Insert { get; set; }
+
+    /// <summary>
+    ///     The percentage of operations in the mix that are removals.
+    /// </summary>
+    public byte Remove { get; set; }
+
+    /// <summary>
+    ///     The percentage of operations in the mix that are updates.
+    /// </summary>
+    public byte Update { get; set; }
+
+    /// <summary>
+    ///     The percentage of operations in the mix that are update-or-inserts.
+    /// </summary>
+    public byte Upsert { get; set; }
+
+    /// <summary>
+    ///     Constructs a very read-heavy workload (~95%), with limited concurrent modifications.
+    /// </summary>
+    public static Mix ReadHeavy()
+    {
+        return new Mix
+        {
+            Read = 95,
+            Insert = 2,
+            Update = 1,
+            Remove = 1,
+            Upsert = 1
+        };
+    }
+
+    /// <summary>
+    ///     Constructs a read-only workload.
+    /// </summary>
+    public static Mix ReadOnly()
+    {
+        return new Mix
+        {
+            Read = 100,
+            Insert = 0,
+            Update = 0,
+            Remove = 0,
+            Upsert = 0
+        };
+    }
+
+    internal static void Shuffle(List<Operation> opMix)
+    {
+        var rng = new Random();
+        var n = opMix.Count;
+
+        while (n > 1)
+        {
+            n--;
+            var k = rng.Next(n + 1);
+            (opMix[k], opMix[n]) = (opMix[n], opMix[k]);
+        }
+    }
+
+    public List<Operation> ToOps()
+    {
+        var list = new List<Operation>(100);
+        list.AddRange(Enumerable.Repeat(Operation.Read, Read));
+        list.AddRange(Enumerable.Repeat(Operation.Insert, Insert));
+        list.AddRange(Enumerable.Repeat(Operation.Remove, Remove));
+        list.AddRange(Enumerable.Repeat(Operation.Update, Update));
+        list.AddRange(Enumerable.Repeat(Operation.Upsert, Upsert));
+        Shuffle(list);
+
+        return list;
+    }
+}
+
+public enum Operation
+{
+    Read,
+    Insert,
+    Remove,
+    Update,
+    Upsert
+}
+
+internal class Bench
+{
+    private static int RunOps(ConcurrentDictionary<KeyType, ValueType> dict, Keys keys, List<Operation> opMix,
+        int opsPerThread)
+    {
+        var random = new Random(Thread.CurrentThread.ManagedThreadId);
+        var opMixCount = opMix.Count;
+        var totalSuccess = 0;
+
+        // Main loop
+        for (var i = 0; i < opsPerThread; i++)
+        {
+            var op = opMix[i % opMixCount];
+            var r = random.Next();
+            var success = false;
+
+            switch (op)
+            {
+                case Operation.Read:
+                {
+                    success = dict.ContainsKey(keys.Random(r));
+                    break;
+                }
+
+                case Operation.Insert:
+                {
+                    success = dict.TryAdd(keys.Alloc(), 0UL);
+                    break;
+                }
+
+                case Operation.Remove:
+                {
+                    success = dict.TryRemove(keys.Random(r), out var removedVal);
+                    break;
+                }
+
+                case Operation.Update:
+                {
+                    success = dict.TryGetValue(keys.Random(r), out var existingValue) &&
+                              dict.TryUpdate(keys.Random(r), existingValue + 1, existingValue);
+                    break;
+                }
+
+                case Operation.Upsert:
+                {
+                    success = dict.AddOrUpdate(keys.Random(r), 1, (k, oldValue) => oldValue + 1) == 1;
+                    break;
+                }
+            }
+
+            totalSuccess += success ? 0 : 1;
+        }
+
+        return totalSuccess;
+    }
+
+    public static Measurement RunWorkload(List<Operation> operations, RunConfig config, Keys keys, bool runChecks)
+    {
+        var numThreads = config.Threads;
+        var dict = new ConcurrentDictionary<KeyType, ValueType>(numThreads, config.InitialCapacity);
+
+        Console.WriteLine($"start {numThreads} threads");
+
+        keys.Reset();
+        for (var i = 0; i < config.Prefill; i++) dict.TryAdd(keys.Alloc(), 0UL);
+
+        var barrier = new Barrier(numThreads + 1);
+        var tasks = new List<Task>();
+        var opsPerThread = config.TotalOps / numThreads;
+        var elapsedMilliseconds = new ConcurrentBag<long>();
+        var realTotalOps = opsPerThread * numThreads;
+
+        for (var i = 0; i < numThreads; i++)
+            tasks.Add(Task.Run(() =>
+            {
+                barrier.SignalAndWait();
+                var start = Stopwatch.StartNew();
+                RunOps(dict, keys, operations, opsPerThread);
+                start.Stop();
+                elapsedMilliseconds.Add(start.ElapsedMilliseconds);
+            }));
+
+        barrier.SignalAndWait();
+        Task.WaitAll(tasks.ToArray());
+
+        var totalMilliseconds = elapsedMilliseconds.Sum();
+        var avgLatency = (long)totalMilliseconds * 1_000_000 / realTotalOps;
+        Console.WriteLine(
+            $"config complete in {totalMilliseconds} milliseconds (ops: {realTotalOps}, avg: {avgLatency} ns)");
+
+        return new Measurement
+        {
+            TotalOps = realTotalOps,
+            Latency = avgLatency,
+            ThreadCount = numThreads
+        };
+    }
+
+    
+
+    internal struct RunConfig
+    {
+        public int Threads { get; set; }
+        public int InitialCapacity { get; set; }
+        public int TotalOps { get; set; }
+        public int Prefill { get; set; }
+    }
+
+    internal class Keys
+    {
+        private int _allocated;
+        private readonly KeyType[] _keys;
+
+        public Keys(int totalKeys)
+        {
+            var random = new Random();
+            var uniqueSet = new HashSet<KeyType>();
+            while (uniqueSet.Count < totalKeys) uniqueSet.Add((KeyType)random.NextInt64());
+            _keys = uniqueSet.ToArray();
+        }
+
+        internal void Reset()
+        {
+            _allocated = 0;
+        }
+
+        public KeyType Random(int i)
+        {
+            return _keys[i % _allocated];
+        }
+
+        public KeyType Alloc()
+        {
+            var i = Interlocked.Increment(ref _allocated);
+            return _keys[i - 1];
+        }
+    }
+}
