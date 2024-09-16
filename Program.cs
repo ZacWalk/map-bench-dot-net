bd270002ad1b69c3a1651ee41cb7b28155f0af72
Zac Walker <zac@diffractor.com>
2024-09-16 10:27:03 +0200

    Output more data for 100% read

--- Program.cs
+++ Program.cs
@@ -1,89 +1,73 @@
 ﻿using map_bench_dot_net;
 using System.Runtime;
 
-var read99 = Mix.Read99();
-var max99 = read99.ToOps();
-var capacity = 1_000_000;
-var totalOps = capacity * 55;
-var prefill = capacity / 2;
-var keys_needed_for_inserts = (totalOps * read99.Insert / 100) + 1000;
-var totalKeys = prefill + keys_needed_for_inserts + 1000; // 1000 needed for some rounding error?
+Dictionary<string, List<Measurement>> measurements = new Dictionary<string, List<Measurement>>();
 
-List<Measurement> measurements99 = new();
-List<Measurement> measurements100 = new();
-var keys = new Bench.Keys(totalKeys);
+measurements["PERF_DATA_DOT_NET_99_10k"] = RunTest(Mix.Read99(), 10000);
+measurements["PERF_DATA_DOT_NET_99_1M"] = RunTest(Mix.Read99(), 1000000);
+measurements["PERF_DATA_DOT_NET_100_10K"] = RunTest(Mix.ReadOnly(), 10000);
+measurements["PERF_DATA_DOT_NET_100_1M"] = RunTest(Mix.ReadOnly(), 1000000);
 
-for (var numThreads = 0; numThreads < Environment.ProcessorCount; numThreads++)
+WriteResults(measurements);
+
+List<Measurement> RunTest(Mix mix, int num_start_items)
 {
-    var config = new Bench.RunConfig
+    var ops = mix.ToOps();
+    var totalOps = 40000000;
+    var prefill = num_start_items;
+    var expected_inserts = (totalOps * mix.Insert / 100);
+    var capacity = num_start_items + expected_inserts;
+    var totalKeys = prefill + expected_inserts + 1000; // 1000 needed for some rounding error?
+
+    List<Measurement> measurements = new();
+    var keys = new Bench.Keys(totalKeys);
+
+    for (var numThreads = 0; numThreads < Environment.ProcessorCount; numThreads++)
     {
-        Threads = numThreads + 1,
-        InitialCapacity = capacity,
-        TotalOps = totalOps,
-        Prefill = prefill
-    };
+        var config = new Bench.RunConfig
+        {
+            Threads = numThreads + 1,
+            InitialCapacity = capacity,
+            TotalOps = totalOps,
+            Prefill = prefill
+        };
 
-    Console.Write($" 99% read {numThreads,2} threads ... ");
-    var keysPerThread = keys_needed_for_inserts / config.Threads;
-    var measurement = Bench.RunWorkload(max99, config, keys, keysPerThread);
-    measurements99.Add(measurement);
-    Console.WriteLine($"avg: {measurement.AvLatency} ns");
-}
-
-var read100 = Mix.ReadOnly();
-var mix100 = read100.ToOps();
-
-for (var numThreads = 0; numThreads < Environment.ProcessorCount; numThreads++)
-{
-    var config = new Bench.RunConfig
-    {
-        Threads = numThreads + 1,
-        InitialCapacity = capacity,
-        TotalOps = totalOps,
-        Prefill = prefill
-    };
-
-    Console.Write($"100% read {numThreads,2} threads ... ");
-    var keysPerThread = keys_needed_for_inserts / config.Threads;
-    var measurement = Bench.RunWorkload(mix100, config, keys, keysPerThread);
-    measurements100.Add(measurement);
-    Console.WriteLine($"avg: {measurement.AvLatency} ns");
-}
-
-var rustFile = "perf_dotnet_data.rs";
-
-using (var writer = new StreamWriter(rustFile))
-{
-    writer.WriteLine("use std::sync::LazyLock;");
-    writer.WriteLine("use crate::perf::Measurement;");
-    writer.WriteLine("");
-    writer.WriteLine("pub static PERF_DATA_DOT_NET_99: LazyLock<Vec<Measurement>> = LazyLock::new(|| {  vec![");
-
-    foreach (var m in measurements99)
-    {
-        var code_line = string.Format("Measurement {{ name: \"c#\", thread_count: {0}, latency: {1} }},",
-            m.ThreadCount,
-            m.AvLatency
-        );
-
-        writer.WriteLine(code_line);
+        Console.Write($" {mix.Read}% {num_start_items,7} {numThreads,2} threads ... ");
+        var keysPerThread = expected_inserts / config.Threads;
+        var measurement = Bench.RunWorkload(ops, config, keys, keysPerThread);
+        measurements.Add(measurement);
+        Console.WriteLine($"avg: {measurement.AvLatency} ns");
     }
 
-    writer.WriteLine("] });");
-
-    writer.WriteLine("pub static PERF_DATA_DOT_NET_100: LazyLock<Vec<Measurement>> = LazyLock::new(|| {  vec![");
-
-    foreach (var m in measurements100)
-    {
-        var code_line = string.Format("Measurement {{ name: \"c#\", thread_count: {0}, latency: {1} }},",
-            m.ThreadCount,
-            m.AvLatency
-        );
-
-        writer.WriteLine(code_line);
-    }
-
-    writer.WriteLine("] });");
+    return measurements;
 }
 
-Console.WriteLine("Results file written successfully!");
+void WriteResults(Dictionary<string, List<Measurement>> measurements)
+{
+    var rustFile = "perf_dotnet_data.rs";
+
+    using (var writer = new StreamWriter(rustFile))
+    {
+        writer.WriteLine("use std::sync::LazyLock;");
+        writer.WriteLine("use crate::perf::Measurement;");
+        writer.WriteLine("");
+
+        foreach (var mm in measurements)
+        {
+            writer.WriteLine($"pub static {mm.Key}: LazyLock<Vec<Measurement>> = LazyLock::new(|| {{  vec![");
+
+            foreach (var m in mm.Value)
+            {
+                var code_line = string.Format("Measurement {{ name: \"c#\", thread_count: {0}, latency: {1} }},",
+                    m.ThreadCount,
+                    m.AvLatency
+                );
+
+                writer.WriteLine(code_line);
+            }
+
+            writer.WriteLine("] });");
+        }
+    }
+
+}
